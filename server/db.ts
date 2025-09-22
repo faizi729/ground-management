059e4e87719b996da19c879a5f6482c5a421de3f
faizi729 <f68857440@gmail.com>
2025-09-23 01:00:59 +0530

    third commit

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,15 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import pkg from 'pg';
-
+import dns from 'dns';
 const { Pool } = pkg;
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL must be set. Did you forget to provision a database?');
 }
-
+dns.setDefaultResultOrder('ipv4first');
 const pool = new Pool({ connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  family: 4  // Use IPv4, skip trying IPv6
+
+  ssl: { rejectUnauthorized: false }
  });
 export const db = drizzle(pool);
